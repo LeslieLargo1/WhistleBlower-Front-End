f5cd39c9a3997530c277603d9fa4abbdf485c1ba
latteflo <sf.simion.f@gmail.com>
2023-09-11 15:06:00 +0200

    added navigation + responsiveness, footer and homepage

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react"
+import "./styles/style.css"
 
 const Homepage = () => {
-  return <h2>Homepage</h2>;
-};
+  return (
+    <>
+      <div className="hero-image">
+        <div className="overlay"></div>
+      </div>
+      <div className="home-page">
+        <div className="content-container">
+          <h1 className="title">Whistleblower</h1>
+          <p className="info-text">
+            Unlawful activities and abuse of law may occur in any organization.
+            Be the change and take action now.
+          </p>
+          <div className="action-section">
+            <button className="action-button">
+              <p className="action-text">Take Action</p>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
 
-export default Homepage;
+export default Homepage
