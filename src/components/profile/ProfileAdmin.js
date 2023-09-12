4a47f251fd0bc4f3cfba5378d286d1a378a90635
latteflo <sf.simion.f@gmail.com>
2023-09-12 19:01:13 +0200

    added 2 more pages and changed register form

--- src/components/profile/ProfileAdmin.js
+++ src/components/profile/ProfileAdmin.js
@@ -1,58 +1,87 @@
-import React from 'react';
-import "./styles/style.css";
+import React, { useEffect, useState } from "react"
+import { useAuth } from "../AuthContext/AuthContext"
+import { useNavigate } from "react-router-dom"
 
-const mockUserData = {
-  id: 6,
-  username: "Leslie",
-  email: "leslie@example.com",
-  role: "client",
-  // Add other mock user data here
-};
+const AdminProfile = () => {
+  const { token } = useAuth()
+  const [userData, setUserData] = useState(null)
+  const navigate = useNavigate()
 
-export const ProfileAdmin = () => {
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const myHeaders = new Headers()
+      myHeaders.append("Authorization", `Bearer ${token}`)
+
+      const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+      }
+
+      try {
+        const response = await fetch(
+          "https://whistle-blower-server.vercel.app/admins/profile",
+          requestOptions
+        )
+        const data = await response.json()
+        setUserData(data)
+      } catch (error) {
+        console.error("Failed to fetch user data:", error)
+      }
+    }
+
+    fetchUserData()
+  }, [token])
+
+  const handleNavigateToCreateNewAdmin = () => {
+    navigate("/create-new-admin")
+  }
+
+  const handleNavigateToChangePassword = () => {
+    navigate("/change-password")
+  }
+
+  const handleLogout = async () => {
+    const myHeaders = new Headers()
+    myHeaders.append("Authorization", `Bearer ${token}`)
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+    }
+
+    try {
+      const response = await fetch(
+        "https://whistle-blower-server.vercel.app/users/logout",
+        requestOptions
+      )
+      const data = await response.json()
+
+      if (data.message === "Logged out successfully") {
+        navigate("/login")
+      }
+    } catch (error) {
+      console.log("Error during logout:", error)
+    }
+  }
+
   return (
-    <div className="profile-ADMIN">
-      <div className="div">
-        <img className="logowhistleblower" alt="logoWhistleblower" src="./styles/logoWhistleblower.png" />
-        <div className="overlap-group">
-          {/* Your navigation elements */}
-        </div>
-        <div className="overlap">
-          <div className="flexcontainer">
-            <p className="text">
-              <span className="text-wrapper-3">Welcome,&nbsp;</span>
-              <span className="text-wrapper-4">{mockUserData.username}</span>
-            </p>
-            <p className="text">
-              <span className="text-wrapper-5">{mockUserData.role}</span>
-            </p>
-          </div>
-        </div>
-        <div className="line" alt="Line" src="./styles/line-1.svg" />
-        <div className="flexcontainer-2">
-          <p className="p">
-            <span className="text-wrapper">Name:</span>
-            <span className="text-wrapper-6">
-              {mockUserData.username}
-              <br />
-            </span>
-          </p>
-          <p className="p">
-            <span className="text-wrapper">Email:</span>
-            <span className="text-wrapper-6"> {mockUserData.email} </span>
-          </p>
-        </div>
-        <div className="text-wrapper-7">Personal info:</div>
-        <div className="img" alt="Line" src="./styles/line-1.svg" />
-        <div className="div-wrapper">
-          <button className="text-wrapper-8">Inbox</button>
-        </div>
-        <div className="overlap-3">
-          <button className="text-wrapper-10">Change password</button>
-        </div>
+    <div className="profile-page">
+      <div className="profile">
+        <h1>Admin Profile</h1>
+        <p>Username: {userData?.username}</p>
+        <p>Role: {userData?.role}</p>
+      </div>
+      <div className="buttons">
+        <button onClick={handleNavigateToCreateNewAdmin}>
+          Create New Admin
+        </button>
+        <button onClick={handleNavigateToChangePassword}>
+          Change Password
+        </button>
+        <button onClick={handleLogout}>Log out</button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProfileAdmin;
+export default AdminProfile
--- src/components/profile/ProfileClient.js
+++ src/components/profile/ProfileClient.js
@@ -1,7 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from "react"
+import ChangePassword from "./ChangePassword"
+import { useAuth } from "../AuthContext/AuthContext"
+import "./styles/style.css"
+import { useNavigate } from "react-router-dom"
 
-const ProfileClient = () => {
-  return <h2>Profile Client</h2>;
-};
+const ClientProfile = () => {
+  const { token, logout } = useAuth()
+  const [userData, setUserData] = useState(null)
+  const navigate = useNavigate()
 
-export default ProfileClient;
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const myHeaders = new Headers()
+      myHeaders.append("Authorization", `Bearer ${token}`)
+
+      const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+      }
+
+      try {
+        const response = await fetch(
+          "https://whistle-blower-server.vercel.app/users/profile",
+          requestOptions
+        )
+        const data = await response.json()
+        setUserData(data)
+      } catch (error) {
+        console.error("Failed to fetch user data:", error)
+      }
+    }
+
+    fetchUserData()
+  }, [token])
+
+  const handleLogout = async () => {
+    const myHeaders = new Headers()
+    myHeaders.append("Authorization", `Bearer ${token}`)
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+    }
+
+    try {
+      const response = await fetch(
+        "https://whistle-blower-server.vercel.app/users/logout",
+        requestOptions
+      )
+      const data = await response.json()
+
+      if (data.message === "Logged out successfully") {
+        logout()
+        navigate("/login")
+      }
+    } catch (error) {
+      console.log("Error during logout:", error)
+    }
+  }
+
+  const handleNavigateToChangePassword = () => {
+    navigate("/change-password")
+  }
+
+
+  return (
+    <div className="profile-page">
+      <div className="profile">
+        <h1>Profile</h1>
+        <p>Username: {userData?.username}</p>
+        <p>Role: {userData?.role}</p>
+        <div className="buttons">
+          <button onClick={handleLogout}>Log out</button>
+          <button onClick={handleNavigateToChangePassword}>
+            Change Password
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ClientProfile
