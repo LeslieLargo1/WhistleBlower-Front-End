cdb7fc6d61768181e073a111f6d0b84a8353642d
LeslieLargo1 <133000133+LeslieLargo1@users.noreply.github.com>
2023-09-14 16:48:19 +0200

    input fix

--- src/components/about/Aboutaccordion.js
+++ src/components/about/Aboutaccordion.js
@@ -12,19 +12,19 @@ const Aboutaccordion = () => {
         {
             question: "Our Mission",
             answer:
-                "Our Mission is to foster transparency, integrity, and accountability within our organization and the wider community. We are committed to providing a secure and confidential platform for individuals to report wrongdoing, ensuring their voices are heard, and taking decisive action against misconduct.",
+                <p className="aboutPara">Our Mission is to foster transparency, integrity, and accountability within our organization and the wider community. We are committed to providing a secure and confidential platform for individuals to report wrongdoing, ensuring their voices are heard, and taking decisive action against misconduct.</p>,
             imageSrc: "https://images.unsplash.com/photo-1600880292089-90a7e086ee0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
         },
         {
             question: "Values",
             answer:
-                " Integrity - We uphold the highest standards of honesty and ethical conduct. We believe in doing what is right, even when it's difficult.",
+                <p className="aboutPara"> Integrity - We uphold the highest standards of honesty and ethical conduct. We believe in doing what is right, even when it's difficult.</p>,
             imageSrc: "https://images.unsplash.com/photo-1637919649104-c292cbbbd97b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80"
         },
         {
             question: " Whistleblower Protection",
             answer:
-                "We prioritize the safety and well-being of whistleblowers. We provide safeguards to ensure their anonymity and protect them from retaliation.",
+                <p className="aboutPara">We prioritize the safety and well-being of whistleblowers. We provide safeguards to ensure their anonymity and protect them from retaliation.</p>,
             imageSrc: "https://images.unsplash.com/photo-1558021211-6d1403321394?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1326&q=80"
         },
     ]
